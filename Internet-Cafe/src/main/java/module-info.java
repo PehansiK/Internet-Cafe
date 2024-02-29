4b6519289a0840316817b9e382a355dc58eaf15e
PehansiK <pehansiranumi@gmail.com>
2024-02-29 19:03:46 +0530

    Changes

--- Internet-Cafe/src/main/java/module-info.java
+++ Internet-Cafe/src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.example.internetcafe {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.javafx;
+
 
 
     opens com.example.internetcafe to javafx.fxml;
     exports com.example.internetcafe;
+
 }
